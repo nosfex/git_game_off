ec74cb874f93c60068de62a3f5a5a4f7f5a23e58
nosfex <heidel.gerardo@gmail.com>
2019-11-04 22:11:34 -0300

    Character Level Up Component and data asset

---
+++ ggo/Source/ggo/GGODataLevelUp.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "GGODataLevelUp.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GGO_API UGGODataLevelUp : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};

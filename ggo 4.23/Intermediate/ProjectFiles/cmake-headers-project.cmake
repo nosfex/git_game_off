939cff1f9a3cfaa4d7d00509b9df54a1ef2a3861
Gerardo <heidel.gerardo@gmail.com>
2019-11-06 13:50:44 -0300

    Repo cleanup

--- ggo 4.23/Intermediate/ProjectFiles/cmake-headers-project.cmake
+++
@@ -1,6 +0,0 @@
-set(PROJECT_HEADER_FILES 
-	"/home/gerardo/projects/git_game_off/ggo 4.23/Source/ggo/ggo.h"
-	"/home/gerardo/projects/git_game_off/ggo 4.23/Source/ggo/ggoCharacter.h"
-	"/home/gerardo/projects/git_game_off/ggo 4.23/Source/ggo/ggoGameMode.h"
- )
-
